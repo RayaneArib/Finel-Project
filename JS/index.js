0460fa89315395972fc985cb0098e810eca1b86b
Rayane Arib <aribrayane@gmail.com>
2023-04-01 16:40:47 +0100

    Version10

--- JS/index.js
+++ JS/index.js
@@ -1,36 +1,36 @@
-// const bar = document.getElementById('bar');
-// const close = document.getElementById('close');
-// const nav = document.getElementById('navbar');
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
-// if (bar) {
-//     bar.addEventListener('click', () => {
-//         nav.classList.add('active');
-//     })
-// }
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
 
-// if (close) {
-//     close.addEventListener('click', () => {
-//         nav.classList.remove('active');
-//     })
-// }
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
 
 
 
-// var MainImg = document.getElementById("MainImg");
-//             var smallimg = document.getElementsByClassName("small-img");
+var MainImg = document.getElementById("MainImg");
+            var smallimg = document.getElementsByClassName("small-img");
 
-//             smallimg[0].onclick = function() {
-//                 MainImg.src = smallimg[0].src;
-//             } 
-//             smallimg[1].onclick = function() {
-//                 MainImg.src = smallimg[1].src;
-//             } 
-//             smallimg[2].onclick = function() {
-//                 MainImg.src = smallimg[2].src;
-//             } 
-//             smallimg[3].onclick = function() {
-//                 MainImg.src = smallimg[3].src;
-//             }
+            smallimg[0].onclick = function() {
+                MainImg.src = smallimg[0].src;
+            } 
+            smallimg[1].onclick = function() {
+                MainImg.src = smallimg[1].src;
+            } 
+            smallimg[2].onclick = function() {
+                MainImg.src = smallimg[2].src;
+            } 
+            smallimg[3].onclick = function() {
+                MainImg.src = smallimg[3].src;
+            }
 
 
             let carts = document.querySelectorAll('.add-cart');
